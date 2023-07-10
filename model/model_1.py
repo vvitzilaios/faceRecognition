bd9108b80f3953feca9d1f9de852de119ae51f78
Vasilis Vitzilaios <vasileios.vitzilaios@gmail.com>
2023-07-10 03:17:50 +0300

    [Update] Uploaded report in docx and pdf format, changed model 1 to incorporate the new changes of last commit.

--- model/model_1.py
+++ model/model_1.py
@@ -12,14 +12,14 @@ class ModelOne(nn.Module):
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3)    # 3 channels, 64 filters, 3x3 kernel
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3)  # 64 channels, 128 filters, 3x3 kernel
         # Fully connected layers
-        self.fc1 = nn.Linear(128 * 61 * 61, 120)        # 128 * 61 * 61 input features, 120 output features
+        self.fc1 = nn.Linear(128 * 62 * 62, 120)        # 128 * 61 * 61 input features, 120 output features
         self.fc2 = nn.Linear(120, 84)                   # 120 input features, 84 output features
         self.fc3 = nn.Linear(84, self.num_classes)      # 84 input features, num_classes output features
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))            # Convolution 1, ReLU activation, max pooling
         x = self.pool(F.relu(self.conv2(x)))            # Convolution 2, ReLU activation, max pooling
-        x = x.view(-1, 128 * 61 * 61)                   # Flatten the output of the previous layer
+        x = x.view(-1, 128 * 62 * 62)                   # Flatten the output of the previous layer
         x = F.relu(self.fc1(x))                         # Fully connected layer, ReLU activation
         x = F.relu(self.fc2(x))                         # Fully connected layer, ReLU activation
         x = self.fc3(x)                                 # Fully connected layer
